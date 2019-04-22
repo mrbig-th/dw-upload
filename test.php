d62773c5287fc58992cdb67276f4bc7e1a4b6010
Mr.Sittichai Raksasuk <dwthai@gmail.com>
2019-04-22 20:46:18 +0700

    Edit test.php

--- test.php
+++ test.php
@@ -1,12 +1,12 @@
 <?php
-include("src/DwUpload.php");
+include("vendor/autoload.php");
 use DwThai\DwUpload;
 if(!empty($_FILES['image'])){
-	$str = DwUpload::DwUpload($_FILES['image'],'img/',true);
+	$str = DwUpload::DwUpload($_FILES['image'],'./',true);
 	echo $str;
 }
 ?>
-<form action="" method="post" enctype="multipart/form-data">
+<form method="post" enctype="multipart/form-data" action="">
         <input type="file" name="image" value=""/>
         <input type="submit" name="btn" value="Upload Now !"/>
 </form>
